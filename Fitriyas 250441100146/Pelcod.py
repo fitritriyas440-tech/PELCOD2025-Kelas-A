86504f7887247388624d8ac0c9d996131c3cd92b
fitritriyas440-tech <fitritriyas440@gmail.com>
2025-09-12 21:40:02 -0700

    PELCOD2025-Kelas-A

---
+++ Fitriyas 250441100146/Pelcod.py
@@ -0,0 +1,33 @@
+print("nama saya fitriyas")
+print("="*50)
+nama = "fitriyas"
+print(nama)
+print(type(nama))
+boolean= True
+print(type(boolean))
+print("="*50)
+penjumlahan = 5+7
+print("hasilnya adalah",penjumlahan)
+nama_asprak= "andika kamal"
+nama_calon_dosen=(nama_asprak)
+print(nama_calon_dosen)
+
+# tipe data number
+# tahun_lahir = int(input("masukkan tahun lahir:"))
+# tahun_sekarang =int(input)("masukkan tahun sekarang :")
+# perhitungan_usia = tahun_sekarang-tahun_lahir
+# print(perhitungan_usia)
+
+# bilangan ="51"
+# bilangan_2="7"
+# modulus = int(bilangan)&int(bilangan_2)
+# print(modulus)
+
+# cek =bilangan > bilangan_2
+# print(cek)
+
+saldo = float(input("masukkan saldo awal :"))
+deposit = float(input("masukkan saldo deposit :"))
+saldo += deposit
+print(saldo)
+
